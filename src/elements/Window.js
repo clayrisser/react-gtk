2513bf8841ae8ed0f55fa9053735126679a69efc
Jam Risser <jam@codejam.ninja>
2019-03-03 16:20:53 -0600

    Create get proptype meta logic

--- src/elements/Window.js
+++ src/elements/Window.js
@@ -1,7 +1,60 @@
+import PropTypes from 'prop-types';
 import Gtk from '../gtk';
 import Element from './Element';
 
 export default class Window extends Element {
+  static propTypes = {
+    acceptFocus: PropTypes.bool,
+    application: PropTypes.object,
+    attachedTo: PropTypes.object,
+    decorated: PropTypes.bool,
+    defaultHeight: PropTypes.number,
+    defaultWidth: PropTypes.number,
+    deletable: PropTypes.bool,
+    destroyWithParent: PropTypes.bool,
+    focusOnMap: PropTypes.bool,
+    focusVisible: PropTypes.bool,
+    gravity: PropTypes.number,
+    hasResizeGrip: PropTypes.bool,
+    hasToplevelFocus: PropTypes.func,
+    hideTitlebarWhenMaximized: PropTypes.bool,
+    icon: PropTypes.object,
+    iconName: PropTypes.string,
+    isActive: PropTypes.func,
+    isMaximized: PropTypes.func,
+    mnemonicsVisible: PropTypes.bool,
+    modal: PropTypes.bool,
+    resizable: PropTypes.bool,
+    resizeGripVisible: PropTypes.bool,
+    role: PropTypes.string
+  };
+
+  static defaultProps = {
+    acceptFocus: null,
+    application: null,
+    attachedTo: null,
+    decorated: null,
+    defaultHeight: null,
+    defaultWidth: null,
+    deletable: null,
+    destroyWithParent: null,
+    focusOnMap: null,
+    focusVisible: null,
+    gravity: null,
+    hasResizeGrip: null,
+    hasToplevelFocus: null,
+    hideTitlebarWhenMaximized: null,
+    icon: null,
+    iconName: null,
+    isActive: null,
+    isMaximized: null,
+    mnemonicsVisible: null,
+    modal: null,
+    resizable: null,
+    resizeGripVisible: null,
+    role: null
+  };
+
   constructor(props = {}) {
     super(
       new Gtk.Window({

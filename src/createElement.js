446bc31d5e24de045ef7bb5f8dd142f4583ab891
Jam Risser <jam@codejam.ninja>
2019-03-04 15:54:42 -0600

    Refactored for lerna

--- src/createElement.js
+++
@@ -1,7 +0,0 @@
-import elements from './elements';
-
-export default function createElement(type, props) {
-  const Element = elements[type];
-  if (!Element) throw new Error(`unknown element of type '${type}'`);
-  return new Element(props);
-}

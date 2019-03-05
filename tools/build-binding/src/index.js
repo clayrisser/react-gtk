530e6c74f904403dbde3321e5e97a2989c2320cc
Jam Risser <jam@codejam.ninja>
2019-03-05 05:28:38 -0600

    Created gtk gir

--- tools/build-binding/src/index.js
+++ tools/build-binding/src/index.js
@@ -1,1 +1,10 @@
-console.log('hi');
+import _ from 'lodash';
+import GtkGir from './GtkGir';
+
+async function main() {
+  const gtkGir = new GtkGir();
+  await gtkGir.init();
+  console.log(gtkGir.namespaces);
+}
+
+main();

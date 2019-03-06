dc371f446aa0ad570e14a400d6cac75ee24c6a40
Jam Risser <jam@codejam.ninja>
2019-03-06 06:45:21 -0600

    Migrated reconciler to typescript

--- packages/binding/src/createElement.ts
+++ packages/binding/src/createElement.ts
@@ -1,9 +1,9 @@
-type TElement = any;
+import elements from './elements';
 
-const elements: { [key: string]: TElement[] } = {};
-
-export default function createElement(type: string, props: any) {
-  const Element: TElement = elements[type];
-  if (!Element) throw new Error(`unknown element of type '${type}'`);
+export default function createElement(elementType: string, props: any) {
+  const Element = elements[elementType];
+  if (!Element) {
+    throw new Error(`unknown element of type '${elementType}'`);
+  }
   return new Element(props);
 }
--- packages/binding/src/gtk.ts
+++ packages/binding/src/gtk.ts
@@ -2,7 +2,7 @@ import nodeGtk from 'node-gtk';
 
 let initialized = false;
 
-function getGtk(version: string = '3.0') {
+function getGtk(version = '3.0') {
   const Gtk = nodeGtk.require('Gtk', version);
   if (!initialized) {
     initialized = true;
